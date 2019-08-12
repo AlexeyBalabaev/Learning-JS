6739b53dfe739cd360f816d411ba6eb6be1b381e
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-08-12 15:47:50 +0300

    lesson 6

--- lesson_06/script.js
+++ lesson_06/script.js
@@ -1,1 +1,17 @@
-alert( "HELLO!!! Greatful world!" );
+"use strict";
+
+/* Operators */
+
+let str = "My" + "string!";
+alert( str );   // Mystring!
+
+
+let one = -2;
+alert( +one );    // -2
+alert( +true );   // 1
+alert( +"" );     // 0
+
+let apples = 2;
+let oranges = 3;
+alert( apples + oranges );   // 23
+alert( +apples + +oranges ); // 5
