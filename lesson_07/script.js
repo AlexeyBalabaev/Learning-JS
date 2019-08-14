fcfcab8349f290ebb09940b0464d2a093ef65d7e
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-08-14 15:54:40 +0300

    lesson 7

---
+++ lesson_07/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/* compare */
+alert( 2 > 1 );     // true
+alert( 2 == 1 );    // false
+alert( 2 != 1 );    // true
+
+alert( 'Z' > 'A' );       // true
+alert( 'Dog' > 'Doc' );   // true
+alert( 'Cats' > 'Cat' );  // true
+
+alert( '2' > 1 );    // true
+alert( '01' == 1 );  // true
+
+alert( true == 1 );    // true
+alert( false == 0 );   // true
+
+alert( 0 === false );  // false
+
+alert( null === undefined );  // false
+alert( null == undefined );   // true
