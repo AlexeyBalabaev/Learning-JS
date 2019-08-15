922b6941bb615d7a4bcbc565f79831c7f09d2cc3
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-08-15 15:44:16 +0300

    lesson 8

---
+++ lesson_09/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/* alert */
+alert( "Hello!" );
+
+/* prompt */
+let age = prompt( 'How old are you?', 100 );
+alert( `You're ${age} years old!` );
+
+/* confirm */
+let isBoss = comfirm( 'Are you boss here?' );
+alert( isBoss );
