3b390be558f6523e3569e8b11550e7b713a0d63d
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-08-16 23:09:44 +0300

    lesson 9

---
+++ lesson_09/task1.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let answer = prompt( "What's the official name JavaScript?", '' );
+
+if( answer == 'ECMAScript' ) {
+	alert( "You're right!" );
+} else {
+	alert( "You don't know? ECMAScript!" );
+}
