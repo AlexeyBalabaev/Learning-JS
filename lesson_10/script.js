9eacc82bb5b7835612a0c93db5378c994a608168
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-08-17 22:32:07 +0300

    lesson 10

--- lesson_10/script.js
+++ lesson_10/script.js
@@ -1,13 +1,13 @@
 "use strict";
 
-/* Logical */
+/* Logical. OR ( || ) */
 alert( true || true );    // true
 alert( false || true );   // true
 alert( true || false );   // true
 alert( false || false );  // false
 
 /* ----- */
-if( 1 || 0 ) {            // work like if(true || false)
+if (1 || 0) {            // work like if(true || false)
   alert( 'Truthy!' );
 };
 
@@ -15,6 +15,40 @@ if( 1 || 0 ) {            // work like if(true || false)
 let hour = 12;
 let isWeekend = true;
 
-if(hour < 10 || hour > 18 || isWeekend) {
-  alert( 'Ofice closed.' );    // Weekend
+if (hour < 10 || hour > 18 || isWeekend) {
+  alert( 'Office closed.' );    // Weekend
 };
+
+/* ----- */
+alert( null || 0 || 1 );          // 1
+alert( undefined || null || 0 );  // 0
+
+/* ----- */
+let currentUser = null;
+let defaulUser = "Jhon";
+
+let name = currentUser || defaulUser || "unnamed";
+
+alert( name );   // "Jhon"
+
+/* AND ( && ) */
+alert( true || true );    // true
+alert( false || true );   // false
+alert( true || false );   // false
+alert( false || false );  // false
+
+/* ----- */
+let hourToday = 12;
+let minuteToday = 30;
+
+if (hourToday == 12 && minuteToday == 30) {
+	alert( 'THe time is 12:30' );
+};
+
+/* ----- */
+alert( 1 && 0 );                // 0
+alert( 1 && 5 );                // 5
+alert( null && 5 );             // null
+alert( 0 && 'no matter what' ); // 0
+alert( 1 && 2 && null && 3 );   // null
+alert( 1 && 2 && 3 );           // 3
