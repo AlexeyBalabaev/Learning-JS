11a905b5ce434c128c063bf2b3a9859bae99c1c9
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-08-22 00:42:03 +0300

    lesson 13

--- lesson_13/script.js
+++ lesson_13/script.js
@@ -28,3 +28,80 @@
 };
 
 /* ----- */
+{
+  let userName = 'Jhon';
+
+  function showMessage() {
+     let userName = 'Ben';
+
+    let message = 'Hi, ' + userName;
+    alert( message );   // Ben
+  };
+
+  showMessage();
+
+  alert( userName );    // Jhon
+};
+
+/* ----- */
+{
+  function showMessage(from, message) {
+    from = '*' + from + '*';
+
+    alert( from + ': ' + message );
+  };
+
+  let from = 'Ann';
+
+  showMessage(from, 'Hello!');        // *Ann*: Hello!
+  showMessage(from, 'How are you?');  // *Ann*: How are you?
+
+  alert( from );     // Ann
+};
+
+/* ----- */
+{
+  function showMessage(from, message) {
+    alert( from + ': ' + message );
+  };
+
+  showMessage('Ann');   // Ann: undefined
+};
+
+/* ----- */
+{
+  function showMessage(from, message = 'text - none') {
+    alert( from + ': ' + message );
+  };
+
+  showMessage('Ann');   // Ann: text - none
+};
+
+/* ----- */
+{
+  function sum(a, b) {
+    return a + b;
+  };
+
+  let result = sum(1, 2);
+  alert( result );
+};
+
+/* ----- */
+{
+  function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      return confirm('Your parents allowed?');
+    };
+  };
+
+  let age = prompt('How old are you?', '18');
+
+  if ( checkAge(age) ) {
+    alert( 'Accessed!' );
+  } else {
+    alert( 'Access closed!' );
+  };
+};
