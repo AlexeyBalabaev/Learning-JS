2c061ce11856a34e138039f2b3f5aada00714a2c
AlexeyBalabaev <jhon.snow.verysnow@yandex.ru>
2019-08-25 20:11:51 +0300

    lesson 15

--- lesson_15/script.js
+++ lesson_15/script.js
@@ -2,5 +2,52 @@
 
 // Object
 // Two variantes ctreating Object:
-let user = new Object();
-let user = {};
+{
+  let user1 = new Object();
+  let user2 = {};
+}
+
+/* ----- */
+{
+  let user = {
+    name: "Jhon",
+    age: 30
+  };
+
+  alert( user.name );   // Jhon
+  alert( user.age );    // 30
+
+  user.isAdmin = true;
+
+  delete user.age;
+}
+
+/* ----- */
+{
+  let user = {
+    name: "Jhon",
+    age: 30,
+    "likes birds": true
+  };
+}
+
+/* ----- */
+{
+  let user = {};
+
+  user["likes birds"] = true;
+  alert( user["likes birds"] );    // true
+  delete user["likes birds"];
+}
+
+/* ----- */
+{
+  let user = {
+    name: "Jhon",
+    age: 30,
+  };
+
+  let key = "likes birds";
+
+  user[key] = true;      // the same, that user["likes birds"] = true;
+}
