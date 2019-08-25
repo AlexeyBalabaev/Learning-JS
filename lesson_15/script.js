0d4155e13a922242aec967cbd3039991a43df90f
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-08-26 01:50:16 +0300

    lesson 15

--- lesson_15/script.js
+++ lesson_15/script.js
@@ -51,3 +51,127 @@
 
   user[key] = true;      // the same, that user["likes birds"] = true;
 }
+
+/* ----- */
+{
+  let user = {
+    name: "Jhon",
+    age: 30
+  };
+
+  let key = prompt("Whot do you want to know about user?", "name / age");
+
+  alert( user[key] );
+}
+
+/* ----- */
+{
+  let fruit = prompt("What fruit to buy?", "apple");
+
+  let bag = {
+    [fruit]: 5,
+  };
+
+  alert( bag.apple );
+}
+
+/* ----- */
+{
+  let fruit = 'apple';
+  let bag = {
+    [fruit + 'Computers']: 5,
+  };
+
+  alert( bag.appleComputers );     // 5
+}
+
+
+/* ----- */
+// instead of this:
+{
+  function makeUser(name, age) {
+    return {
+      name: name,
+      age: age,
+    };
+  }
+
+  let user = makeUser("Jhon", 30);
+  alert( user.name );          // Jhon
+}
+
+// we can use this:
+{
+  function amkeUser(name, age) {
+    return {
+      name,         // the same -- name: name,
+      age           // the same -- age: age
+    };
+  }
+}
+
+/* ----- */
+// check property
+{
+  let user = {};
+  alert( user.noSuchProperty === undefined );   // true, it means - "property not"
+}
+
+/* ----- */
+{
+  let user = { name: "Jhon", age: 30 };
+
+  alert( "age" in user );    // true,  user.age  -- exist 
+  alert( "smth" in user );   // false, user.smth -- not exist
+}
+
+/* ----- */
+{
+  let user = { age: 30 };
+
+  let key = "age";
+  alert( key in user );   // true
+}
+
+/* ----- */
+//for..in
+{
+  let user = {
+    name: "Jhon",
+    age: 30,
+    isAdmin: true,
+  };
+
+  for (let key in user) {
+    alert( key );         // name, age, isAdmin
+    alert( user[key] );   // Jhon, 30, true
+  }
+}
+
+/* ----- */
+{
+  let codes = {
+    "49": "Germany",
+    "41": "Switzerland",
+    "44": "Great Britain",
+    "1": "USA",
+  };
+
+  for (let code in codes) {
+    alert( code );     // 1, 41, 44, 49
+  }
+}
+
+// we can add plus ahead codes:
+{
+  let codes = {
+    "+49": "Germany",
+    "+41": "Switzerland",
+    "+44": "Great Britain",
+    "+1": "USA",
+  };
+
+  for (let code in codes) {
+    alert( code );     // 49, 41, 44, 1
+  }
+}
