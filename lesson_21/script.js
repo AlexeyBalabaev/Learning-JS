ab9657ef36cf070531147910c823ab46942ad026
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-09-02 23:33:58 +0300

    lesson 21

--- lesson_21/script.js
+++ lesson_21/script.js
@@ -40,4 +40,115 @@
 }
 
 /* ----- */
-alert( `My\n`.length );    // 3
+{
+  alert( `My\n`.length );    // 3
+}
+
+/* ----- */
+{
+  let str = `Hello`;
+
+  alert( str[0] );         // H
+  alert( str.charAt(0) );  // H
+
+  alert( str[str.length - 1] );  // o
+
+  alert( str[1000] );         // undefined
+  alert( str.charAt(1000) );  // ''
+
+  for (let char of str) {
+    alert(char);    // H, e, l, l, o
+  }
+}
+
+/* ----- */
+{
+  let str = 'Hi';
+
+  /*
+  str[0] = 'h';      <-- error
+  alert( str[0] );   <-- don't work
+  */
+
+  str = 'h' + str[1];
+  alert( str );    // hi
+}
+
+
+/* ----- */
+{
+  alert( 'Interface'.toUpperCase() );   // INTERFACE
+  alert( 'Interface'.toLowerCase() );   // unterface
+
+  alert( 'Interface'[0].toLowerCase() );  // i
+}
+
+/* ----- */
+{
+  let str = 'Widget with id';
+
+  alert( str.indexOf('Widget') );  // 0
+  alert( str.indexOf('widget') );  // -1
+
+  alert( str.indexOf('id') );      // 1
+
+  alert( str.indexOf('id', 2) );   // 12
+}
+
+/* ----- */
+{
+  let str = 'number numb nu number numBer';
+  let target = 'ber';
+  let pos = 0;
+
+  while (true) {
+    let foundPos = str.indexOf(target, pos);
+    if (foundPos == -1) break;
+
+    alert( `Found here: ${foundPos}` );
+    pos = foundPos + 1;
+  }
+}
+// or... the same
+{
+  let str = 'number numb nu number numBer';
+  let target = 'ber';
+  let pos = -1;
+
+  while ((pos = str.indexOf(target, pos + 1)) != -1) {
+    alert( pos );
+  }
+}
+
+/* ----- */
+{
+  let str = "Widget with id";
+
+  if (str.indexOf("Widget") != -1) {    // if (str.indexOf("Widget")) { ... }   <-- don't work
+    alert("There is a coincidence!");
+  }
+
+  if (~str.indexOf("Widget")) {
+    alert("THere is a coincidence!");
+  }
+}
+
+/* ----- */
+{
+  alert( ~2 );   // -3, the same -(2+1)
+  alert( ~1 );   // -2, the same -(1+1)
+  alert( ~0 );   // -1, the same -(0+1)
+  alert( ~-1 );  //  0, the same -(-1+1)
+}
+
+/* ----- */
+{
+  alert( "Widget with id".includes("Widget") );   // true
+  alert( "Hello".includes("Bye") );    // false
+
+  alert( "Midget".includes("id") );    // true
+  alert( "Midget".includes("id", 3) ); // false
+
+  alert( "Widget".startsWith("Wid") );  // true
+  alert( "Widget".endsWith("get") );    // true
+}
