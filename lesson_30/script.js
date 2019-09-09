60a2c96ff714bf3bb340b6b6b95c7c1f7dfa72cc
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-09-09 22:44:58 +0300

    lesson 30

---
+++ lesson_30/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// JSON
+{
+  let user = {
+    name: "John",
+    age: 30,
+
+    toString() {
+      return `${name: "${this.name}", age: ${this.age}}`;
+    }
+  };
+
+  alert(user);   // {name: "John", age: 30}
+}
