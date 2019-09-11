a89e88f84126e7ce2d1f91dc397f9330177087a0
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-09-11 19:41:11 +0300

    lesson 33

---
+++ lesson_33/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+{
+  function makeCounter() {
+    let count = 0;
+    return function() {
+      return count++;
+    };
+  }
+
+  let counter1 = makeCounter();
+  let counter2 = makeCounter();
+
+  alert( counter1() );   // 0
+  alert( counter1() );   // 1
+
+  alert( counter2() );   // 0
+}
---
+++ lesson_33/task2.js
@@ -0,0 +1,7 @@
+"use sctrict";
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+function inBetween(a, b) {
+  
+}
