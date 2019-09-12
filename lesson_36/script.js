9d00709416ddff431dc0d50adb2784d36bafb3de
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-09-12 22:04:49 +0300

    lesson 36

---
+++ lesson_36/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+{
+  let sum = new Function('a', 'b', 'return a + b');
+
+  alert( sum(1, 2) );   // 3
+}
+
+/* ----- */
+{
+  let sayHi = new Function('alert("Hello")');
+
+  sayHi();   // Hello
+}
