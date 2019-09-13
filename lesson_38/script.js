db4db53f1c09677e0295bbce69fd5341f1b5241c
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-09-13 23:26:45 +0300

    lesson 38

--- lesson_38/script.js
+++ lesson_38/script.js
@@ -14,7 +14,7 @@
         return cache.get(x);
       }
 
-      let resule = func(x);
+      let result = func(x);
 
       cache.set(x, result);
       return result;
@@ -28,4 +28,69 @@
 
   alert( slow(2) );   // slow(2)  cache
   alert( "Again: " + slow(2) );   // return cache
+}
+
+/* ----- */
+{
+  func(1, 2, 3);
+  // the same, but second variant has a context
+  func.call(obj, 1, 2, 3);
+}
+
+/* ----- */
+{
+  function sayHi() {
+    alert(this.name);
+  }
+
+  let user = { name: "John" };
+  let admin = { name: "Admin" };
+
+  sayHi.call( user );   // John
+  sayHi.call( admin );  // Admin
+}
+
+/* ----- */
+{
+  function say(phrase) {
+    alert(this.name + ': ' + phrase);
+  }
+
+  let user = { name: "John" };
+
+  say.call( user, "Hello" );    // John: Hello
+}
+
+/* ----- */
+{
+  let worker = {
+    someMethod() {
+      return 1;
+    },
+
+    slow(x) {
+      alert("Called with " + x);
+      return x * this.someMethod();
+    }
+  };
+
+  function cachingDecorator(func) {
+    let cache = new Map();
+
+    return function(x) {
+      if (cache.has(x)) {
+        return cache.get(x);
+      }
+
+      let result = func.call(this, x);
+
+      cache.set(x, result);
+      return result;
+    };
+  }
+
+  worker.slow = cachingDecorator(worker.slow);
+
+  alert(worker.slow(2));   // Called with 2  -->  2
+  alert(worker.slow(2));   // 2
 }
