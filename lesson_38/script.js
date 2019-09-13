f07dbd0113c73dc39b9f2d08fa49ab86364ffe70
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-09-13 13:33:15 +0300

    lesson 38

--- lesson_38/script.js
+++ lesson_38/script.js
@@ -1,2 +1,31 @@
 "use strict";
 
+{
+  function slow(x) {
+    alert(`Called with ${x}`);
+    return x;
+  }
+
+  function cachingDecorator(func) {
+    let cache = new Map();
+
+    return function(x) {
+      if (cache.has(x)) {
+        return cache.get(x);
+      }
+
+      let resule = func(x);
+
+      cache.set(x, result);
+      return result;
+    };
+  }
+
+  slow = cachingDecorator(slow);
+
+  alert( slow(1) );   // slow(1)  cache
+  alert( "Again: " + slow(1) );   // return cache
+
+  alert( slow(2) );   // slow(2)  cache
+  alert( "Again: " + slow(2) );   // return cache
+}
