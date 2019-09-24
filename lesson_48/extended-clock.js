4eb7b355cb995e54715b485145529504adbb98b9
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-09-25 00:36:27 +0300

    lesson 48

--- lesson_48/extended-clock.js
+++ lesson_48/extended-clock.js
@@ -1,21 +1,21 @@
 "use sctrict";
 
 class extendedClock extends Clock {
-	constructor(options) {
-		super(options);
-		let { precision=1000 } = options;
-		this.precision = precision;
-	}
+  constructor(options) {
+    super(options);
+    let { precision=1000 } = options;
+    this.precision = precision;
+  }
 
-	start() {
-		this.render();
-		this.timer = setInterval(() => this.render(), this.precision);
-	}
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
+  }
 };
 
 let lowResolutionClock = new extendedClock({
-	template: 'h:m:s',
-	precision: 10000
+  template: 'h:m:s',
+  precision: 10000
 });
 
 lowResolutionClock.start();
