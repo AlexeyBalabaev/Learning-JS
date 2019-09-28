90b2a035c82071314414a969f23a32368b7e3e5f
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-09-28 09:07:25 +0300

    lesson 51

---
+++ lesson_51/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+{
+  class PowerArray extends Array {
+    isEmpty() {
+      return this.length === 0;
+    }
+  }
+
+  let arr = new PowerArray(1, 2, 5, 10, 50);
+  alert(arr.isEmpty());   // false
+
+  let filteredArr = arr.filter(item => item >= 10);
+  alert(filteredArr);   // 10, 50
+  alert(filteredArr.isEmpty());   // false
+}
+
+/* ----- */
+{
+  class PowerArray extends Array {
+    isEmpty() {
+      return this.length === 0;
+    }
+
+    static get [Symbol.species]() {
+      return Array;
+    }
+  }
+
+  let arr = new PowerArray(1, 2, 5, 10, 50);
+  alert(arr.isEmpty());   // false
+
+  let filteredArr = arr.filter(item => itemm >= 10);
+
+  // alert(filteredArr.isEmpty());  -- Error: filteredArr.isEmpty is not a function
+}
