d2d6dcdb40fef0c408dbefd394a0cb2219b1828c
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-10-02 22:53:21 +0300

    lesson 54

--- lesson_54/script.js
+++ lesson_54/script.js
@@ -55,6 +55,55 @@
 
   } catch(err) {
 
-    alert( "Sorry, error in the data, we'll try to get them again" )
+    alert( "Sorry, error in the data, we'll try to get them again" );
+    alert( err.name );
+    alert( err.message );
+  }
+}
+
+/* ----- */
+{
+  let json = '{ "age": 30 }';
+
+  try {
+
+    let user = JSON.parse(json);
+    alert( user.name );   // undefined -- we have not property "name"
+
+  } catch(e) {
+    alert( "not be fulfilled" );
+  }
+}
+
+/* ----- */
+{
+  let error = new Error("Wow, error!");
+
+  alert(error.name);    // Error
+  alert(error.message); // Wow, error!
+
+  try {
+    JSON.parse("{ bad json }");
+  } catch(e) {
+    alert(e.name);   // SyntaxError
+    alert(e.message);   // Unexpected token o in JSON at position 2
+  }
+}
+
+/* ----- */
+{
+  let json = '{ "age": 30 }';
+
+  try {
+
+    let user = JSON.parse(json);
+
+    if (!user.name) {
+      throw new SyntaxError("Data is incomplete: no name");
+    }
+
+    alert(user.name);
+  } catch(e) {
+    alert("JSON Error: " + e.message);   // JSON Error: Data is incomplete: no name
   }
 }
