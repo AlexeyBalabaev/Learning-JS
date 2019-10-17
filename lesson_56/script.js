6d7d88cc543310f9620e9dd93f3664560533779e
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-10-17 10:20:35 +0300

    lesson 56

--- lesson_56/script.js
+++ lesson_56/script.js
@@ -1,194 +1,66 @@
 "use strict";
 
 {
-  class Validation extends Error {
-    constructor(message) {
-      super(message);
-      this.name = "ValidationError";
-    }
+  function loadScript(src) {
+    let script = document.createElement('script');
+    script.src = src;
+    document.head.append(script);
   }
 
-  function test() {
-    throw new ValidationError("Oops!");
-  }
+  // Example of using
+  loadScript('/my/script.js');
 
-  try {
-    test();
-  } catch(err) {
-    alert(err.message);   // Oops!
-    alert(err.name);      // ValidationError
-    alert(err.stack);
-  }
+  // newFunction();   -- error, this function doesn't exist
 }
 
 /* ----- */
 {
-  class ValidationError extends Error {
-    constructor(message) {
-      super(message);
-      this.name = "ValidationError";
-    }
+  function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+
+    script.onload = () => callback(script);
+
+    document.head.append(script);
   }
 
-  // Using
-  function readUser(json) {
-    let user = JSON.parse(json);
+  loadScript('/my/script.js', function() {
+    newFunction();
+  })
+}
 
-    if (!user.age) {
-      throw new ValidationError("No field: age");
-    }
-    if (!user.name) {
-      throw new ValidationError("No field: name");
-    }
-
-    return user;
+/* ----- */
+{
+  function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.onload = () => callback(script);
+    document.head.append(script);
   }
 
-  // Working example with try..catch
-  try {
-    let user = readUser('{ "age": 25 }');
-  } catch(err) {
+  loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+    alert(`Nice, script ${script.src} is upload`);
+    alert( _ );   // function, declared in loaded script
+  })
+}
 
-    if (err instanceof ValidationError) {
-      alert("Incorrect data: " + err.message);   // Incorrect data: No field: name
-    } else if (err instanceof SyntaxError) {
-      alert("JSON Error of Syntax: " + err.message);
+/* ----- */
+{
+  function loadScript(src, callback) {
+    let script = document.createElement('script');
+    script.src = src;
+
+    script.onload = () => callback(null, script);
+    script.anerror = () => callback(new Error(`Failed to load script ${src}`));
+
+    document.head.append(script);
+  }
+
+  loadScript('/my/script.js', function(error, script) {
+    if (error) {
+      // process the error
     } else {
-      throw err;   // unknown error
+      // script loaded successfully
     }
-
-  }
-}
-
-/* ----- */
-{
-  class ValidationError extends Error {
-    constructor(message) {
-      super(message);
-      this.name = "ValidationError";
-    }
-  }
-
-  class PropertyRequiredError extends ValidationError {
-    constructor(property) {
-      super("No property: " + property);
-      this.name = "PropertyRequiredError";
-      this.property = property;
-    }
-  }
-
-  // Using
-  function readUser(json) {
-    let user = JSON.parse(json);
-
-    if (!user.age) {
-      throw new PropertyRequiredError("age");
-    }
-    if (!user.name) {
-      throw new PropertyRequiredError("name");
-    }
-
-    return user;
-  }
-
-  // Working example with try..catch
-  try {
-    let user = readUser('{ "age": 30 }');
-  } catch(err) {
-
-    if (err instanceof ValidationError) {
-      alert("Incorrect data: " + err.message);   // Incorrect data: No property: name
-      alert(err.name);   // PropertyRequiredError
-      alert(err.property);   // name
-    } else if (err instanceof SyntaxError) {
-      alert("Error of syntax JSON" + err.message);
-    } else {
-      throw err;  // unknown error
-    }
-
-  }
-}
-
-/* ----- */
-{
-  class MyError extends Error {
-    constructor(message) {
-      super(message);
-      this.name = this.constructor.name;
-    }
-  }
-
-  class ValidationError extends MyError { }
-
-  class PropertyRequiredError extends ValidationError {
-    constructor(property) {
-      super("No property: " + property);
-      this.property = property;
-    }
-  }
-
-  alert( new PropertyRequiredError("field").name );   // PropertyRequiredError
-}
-
-/* ----- */
-{
-  class ReadError extends Error {
-    constructor(message, cause) {
-      super(message);
-      this.cause = cause;
-      this.name = "ReadError";
-    }
-  }
-
-  class ValidationError extends Error { /* ... */ }
-  class PropertyRequiredError extends ValidationError { /* ... */ }
-
-  function validateUser(user) {
-    if (!user.age) {
-      throw new PropertyRequiredError("age");
-    }
-
-    if (!user.name) {
-      throw new PropertyRequiredError("name");
-    }
-  }
-
-  function readUser(json) {
-    let user;
-
-    try {
-      user = JSON.parse(json);
-    } catch(err) {
-
-      if (err instanceof SyntaxError) {
-        throw new ReadError("SyntaxError", err);
-      } else {
-        throw err;
-      }
-    }
-
-    try {
-      validateUser(user);
-    } catch(err) {
-
-      if (err instanceof ValidationError) {
-        throw new ReadError("Validation error", err);
-      } else {
-        throw err;
-      }
-    }
-
-  }
-
-  try {
-    readUser('{ bad json }');
-  } catch(e) {
-    if (e instanceof ReadError) {
-      alert(e);
-      alert("Initial error: " + e.cause);
-      // Initial error: SyntaxError:Unexpected token b in JSON at position 1
-    } else {
-      throw e;
-    }
-  }
+  })
 }
