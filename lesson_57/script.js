bf8e5d1354116d4d88beef16302b615758a64f14
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-10-19 10:58:35 +0300

    lesson 57

---
+++ lesson_57/script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+// Promise example (successfully completed promise)
+{
+  let promise  = new Promise(function(resolve, reject) {
+    // this function will be executed automatically when new Promise is called
+
+    // after 1 second to signal that the task is completed with result "done"
+    setTimeout(() => resolve("done"), 1000);
+  });
+}
+
+// Promise example (rejected promise)
+{
+  let promise = new Promise(function(resolve, reject) {
+    // after 1 second it'll be reported that the task completed with an error
+    setTimeout(() => reject(new Error("Whoops!")), 1000);
+  });
+}
+
+/* ----- */
+{
+  let promise = new Promise(function(resolve, reject) {
+    resolve("done");   // will be completed
+
+    reject(new Error("..."));   // ignore
+    setTimeout(() => resolve("..."));   // ignore
+  });
+}
+
+/* ----- */
+{
+  let promise = new Promise(function(resolve, reject) {
+    setTimeout(() => resolve("done!"), 1000);
+  });
+
+  // resolve will start the first function transferred to .then
+  promise.then(
+    result => alert(result), // will display "done!" after 1 second
+    error => alert(error)    // won't start
+  );
+}
+
+/* ----- */
+{
+  let promise = new Promise(function(resolve, reject) {
+    setTTimeout(() => reject(new Error("Whoops!")), 1000);
+  });
+
+  // reject will start the second function transferred to .then
+  promise.then(
+    result => alert(result),  // won't start
+    error => alert(error)     // will display "Error: Whoops!" after 1 second 
+  )
+}
+
+/* ----- */
+{
+  let promise = new Promise(resolve => {
+    setTimeout(() => resolve("done!"), 1000);
+  });
+
+  promise.then(alert);   // will display "done!" after 1 second
+}
