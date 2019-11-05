da6caa433d6f0e773339a37dfc5fb1635fafcfe8
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2019-11-05 16:49:08 +0300

    lesson 61

---
+++ lesson_61/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+{
+  function promisify(f) {
+    return function(...args) {
+      return new Promise((resolve, reject) => {
+        function callback(err, result) {
+          if (err) {
+            return reject(err);
+          } else {
+            resolve(result);
+          }
+        }
+
+        args.push(callback);
+
+        f.call(this, ...args);
+      });
+    };
+  };
+
+  // using
+  // let loadScritPromise = promisify(loadScript);
+  // loadScritPromise(...).then(...);
+}
+
+/* ----- */
+{
+  // promisify(f, true)
+  function promisify(f, manyArgs = false) {
+    return function(...args) {
+      return new Promise((resolve, reject) => {
+        function callback(err, ...results) {
+          if (err) {
+            return reject(err);
+          } else {
+            resolve(manyArgs ? results : results[0]);
+          }
+        }
+
+        args.push(callback);
+
+        f.call(this, ...args);
+      });
+    };
+  };
+
+  // usig
+  // f = promisify(f, true);
+  // f(...).then(arrayOfResults => ..., err => ...)
+}
