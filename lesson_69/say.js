536d4af0a9a7315146e301867f5f6d71ba9654aa
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-01-28 14:19:21 +0300

    lesson 69

---
+++ lesson_69/say.js
@@ -0,0 +1,13 @@
+"use strict";
+
+export function hi() {
+  alert(`Hello!`);
+}
+
+export function bye() {
+  alert(`Bye`);
+}
+
+export default function() {
+  alert('Module loaded (export default)!');
+}
