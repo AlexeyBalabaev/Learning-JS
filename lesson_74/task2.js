fc411367e68cae9822b83c9edbc3e866dfd4c708
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-02-05 15:08:50 +0300

    lesson 74

--- lesson_74/task2.js
+++ lesson_74/task2.js
@@ -1,37 +1,8 @@
 "use strict";
 
-/* ----- */
-for (let i = 0; i < document.body. childNodes.length; i++) {
-  alert( document.body.childNodes[i] );   // Text, DIV, Text, Ul, ..., SCRIPT
+let table = document.body.children(2)
+
+for (let i = 0; i < table.rows.length; i++) {
+  let row = table.rows[i];
+  row.cells[i].style.background = 'red';
 }
-
-/* ----- */
-for (let node of document.body.childNodes) {
-  alert(node);
-}
-
-/* ----- */
-alert( document.body.childNodes.filter );   // undefined
-
-/* ----- */
-alert( Array.from(document.body.childNodes).filter );   // did the array
-
-/* ----- */
-alert( document.body.parentNode === document.documentElement );   // true
-
-alert( document.head.nextSibling );   // HTMLBodyElement
-
-alert( document.body.previousSibling );   // HTMLHeadElement
-
-/* ----- */
-alert( document.documentElement.parentNode );   // will display "document"
-
-alert( document.documentElement.parentElement );   // will display "null"
-
-/* ----- */
-for (let elem of document.body.children) {
-  alert(elem);   // DIV, UL, DIV, SCRIPT
-}
-
-/* ----- */
-alert( table.rows[0].cells[1].innerHTML );   // "two"
