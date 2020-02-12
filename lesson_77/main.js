9ae156fa5dc0988cac67e356aaa2b940ddaae299
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-02-12 18:01:04 +0300

    lesson 77

--- lesson_77/main.js
+++ lesson_77/main.js
@@ -29,3 +29,17 @@ alert(document.body,something);   // undefined
 /* ----- */
 alert(input.type);   // text
 alert(body.type);    // undefined
+
+/* ----- */
+alert(document.body.getAttribute('something'));   // non-standard
+
+/* ----- */
+alert( elem1.getAttribute('About') );   // 'Elephaunt', reading
+
+elem1.setAttribute('Test', 123);   // writing
+
+alert( elem1.outerHTML );   // checking attribute (yes)
+
+for (let attr of elem1.attributes) {
+  alert( `${attr.name} = ${attr.value}` );
+}
