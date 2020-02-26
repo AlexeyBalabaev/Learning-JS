5f1e27e0c05e71e50cbce11d37b56ebfeb9adb54
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-02-26 18:11:33 +0300

    lesson 81

--- lesson_81/main.js
+++ lesson_81/main.js
@@ -1,2 +1,17 @@
 "use strict";
 
+alert( window.innerWidth );
+alert( document.documentElement.clientWidth );
+
+/* ----- */
+let scrollHeight = Math.max(
+  document.body.scrollHeight, document.documentElement.scrollHeight,
+  document.body.offsetHeight, document.documentElement.scrollHeight,
+  document.body.clientHeight, document.documentElement.clientHeight
+);
+
+alert('Full height of document with scrolling part: ' + scrollHeight);
+
+/* ----- */
+alert('Current scroll from top: ' + window.pageYOffset);
+alert('Current scroll from left: ' + window.pageXOffset);
