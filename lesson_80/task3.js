3876d5e912812b0ad539c1e4376881f7a32eca81
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-02-26 12:20:02 +0300

    lessons 80 and 81

--- lesson_80/task3.js
+++ lesson_80/task3.js
@@ -3,5 +3,5 @@
 let field = document.querySelector('.field');
 let ball = document.querySelector('.ball');
 
-ball.getElementsByClassName.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px';
-ball.getElementsByClassName.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px';
+ball.style.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px';
+ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px';
