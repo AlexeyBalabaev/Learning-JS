84ac93311203b2d188e4a3f66c5046a419201d37
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-03-03 17:15:49 +0300

    lesson 83

--- lesson_83/task5.js
+++ lesson_83/task5.js
@@ -1,2 +1,8 @@
 "use strict";
 
+let menuElem = document.querySelector('.menu');
+let titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function() {
+  menuElem.classList.toggle('open');
+};
