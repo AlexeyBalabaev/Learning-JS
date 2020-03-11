f734bf057a18f34cb767f57ce4f02207162876b6
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-03-11 14:55:20 +0300

    lesson 85

--- lesson_85/task4.js
+++ lesson_85/task4.js
@@ -1,2 +1,38 @@
 "use strict";
 
+let task4 = document.querySelector('.task4');
+
+let tooltipElem;
+
+task4.onmouseover = function(event) {
+  let target = event.target;
+
+  let tooltipHtml = target.dataset.tooltip;
+  if (!tooltipHtml) return;
+
+  tooltipElem = document.createElement('div');
+  tooltipElem.className = 'tooltip';
+  tooltipElem.innerHTML = tooltipHtml;
+  task4.append(tooltipElem);
+
+  let coords = target.getBoundingClientRect();
+
+  let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+  if (left < 0) left = 0;
+
+  let top = coords.top - tooltipElem.offsetHeight - 5;
+  if (top < 0) {
+    top = coords.top + target.offsetHeight + 5;
+  }
+
+  tooltipElem.style.left = left + 'px';
+  tooltipElem.style.top = top + 'px';
+};
+
+task4.onmouseout = function(e) {
+
+  if (tooltipElem) {
+    tooltipElem.remove();
+    tooltipElem = null;
+  }
+};
