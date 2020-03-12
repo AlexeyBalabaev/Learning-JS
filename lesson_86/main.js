2edb08fffe636785a40bb762816442926a6ad4fe
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-03-12 10:20:08 +0300

    lesson 86

--- lesson_86/main.js
+++ lesson_86/main.js
@@ -12,9 +12,43 @@ menu.onclick = function(event) {
 }
 
 /* ----- */
-let btnContext = document.querySelector('.btn');
+let btnContext = document.querySelector('.btn-context');
 
 btnContext.oncontextmenu = function(event) {
   event.preventDefault();
   alert("Context menu of button");
+};
+
+document.oncontextmenu = function(event) {
+  event.preventDefault();
+  alert("Context menu of document");
 }
+
+/* ----- */
+let btnContext2 = document.querySelector('.btn-context2');
+
+btnContext2.oncontextmenu = function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  alert("Context menu of button");
+};
+
+document.oncontextmenu = function(event) {
+  event.preventDefault();
+  alert("Context menu of document");
+};
+
+/* ----- */
+let btnContext3 = document.querySelector('.btn-context3');
+
+btnContext3.oncontextmenu = function(event) {
+  event.preventDefault();
+  alert("Context menu of button");
+};
+
+document.oncontextmenu = function(event) {
+  if (event.defaultPrevented) return;
+
+  event.preventDefault();
+  alert("Context menu of document");
+};
