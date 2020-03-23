ed61a55b781cbcc96c40fcf70e06d73786e79c52
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-03-23 16:26:19 +0300

    lesson 87

--- lesson_87/main.js
+++ lesson_87/main.js
@@ -16,21 +16,53 @@ let event2 = new Event("hello", {bubbles: true});
 hOne.dispatchEvent(event2);
 
 /* ----- */
-let event1 = new MouseEvent("click", {
+let event3 = new MouseEvent("click", {
   bubbles: true,
   cancelable: true,
   clientX: 100,
   clientY: 100
 });
 
-alert(event.clientX);   // 100
+alert(event3.clientX);   // 100
 
 /* ----- */
-let event2 = new Event("click", {
+let event4 = new Event("click", {
   bubbles: true,
   cancelable: true,
   clientX: 100,
   clientY: 100
 });
 
-alert(event.clientX);   // undefined
+alert(event4.clientX);   // undefined
+
+/* ----- */
+let elem2 = document.querySelector('.elem2');
+
+elem2.addEventListener("hello", function(event) {
+  alert(event.detail.name);
+});
+
+elem2.dispatchEvent(new CustomEvent("hello", {
+  detail: { name: "John" }
+}));
+
+/* ----- */
+let rabbit = document.querySelector('.rabbit');
+
+function hide() {
+  let event = new CustomEvent("hide", {
+    cancelable: true
+  });
+
+  if (!rabbit.dispatchEvent(event)) {
+    alert("Action is canceled of handler");
+  } else {
+    rabbit.hidden = true;
+  }
+}
+
+rabbit.addEventListener('hide', function(event) {
+  if (confirm("Call preventDefault?")) {
+    event.preventDefault();
+  }
+});
